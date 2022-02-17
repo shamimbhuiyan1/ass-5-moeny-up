d7f9824cc1d790ccc6095cbc193bccffe136005e
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-02-17 20:23:41 +0600

    total expences updated

---
+++ js/sample.js
@@ -0,0 +1,41 @@
+// income part 
+
+    
+    /* const totalIncomeText = totalIncomeInput.value;
+    const totalIncome =parseFloat(totalIncomeText);
+     totalIncomeInput.value=totalIncome;
+ */
+     
+    // expenses part 
+
+
+    // food cost 
+
+    
+    /* const foodInputText = foodInput.value;
+    const foodCost =parseFloat(foodInputText);
+    foodInput.value=foodCost; */
+    
+    // rent cost 
+
+    
+    /* const rentInputText = rentInput.value;
+    const rentCost =parseFloat(rentInputText);
+    foodInput.value=rentCost; */
+
+//    cloths cost 
+
+    
+    /* const clothInputText = clothInput.value;
+    const clothCost =parseFloat(clothInputText);
+    foodInput.value=clothCost; */
+
+
+    // total expances and ramaining balance 
+
+    /* const totalCostText = totalCostInput.innerText;
+    const totalCost = parseFloat(totalCostText);
+    totalCostInput.innerText=totalCost; */
+
+    // update balance 
+    /* const remainingBalance = document.getElementById('rest-balance'); */
--- js/shopup.js
+++ js/shopup.js
@@ -1,35 +1,25 @@
+
+
+const totalIncomeInput=document.getElementById('total-income');
+
+const foodInput = document.getElementById('food-cost');
+
+const rentInput = document.getElementById('rent-cost');
+
+const clothInput = document.getElementById('cloth-cost');
+
 document.getElementById('calculate-button').addEventListener('click',function(){
     
-    // income part 
-
-    const totalIncomeInput=document.getElementById('total-income');
-    const totalIncomeText = totalIncomeInput.value;
-    const totalIncome =parseFloat(totalIncomeText);
-     totalIncomeInput.value=totalIncome;
-
-     totalIncomeInput.value ='';
-    // expenses part 
-
-
-    // food cost 
-
-    const foodInput = document.getElementById('food-cost');
-    const foodInputText = foodInput.value;
-    const foodCost =parseFloat(foodInputText);
-    foodInput.value=foodCost;
     
-    // rent cost 
 
-    const rentInput = document.getElementById('cloth-cost');
-    const rentInputText = rentInput.value;
-    const rentCost =parseFloat(rentInputText);
-    foodInput.value=rentCost;
+    let totalCostInput= document.getElementById('total-cost');
 
-//    cloths cost 
-
-    const clothInput = document.getElementById('cloth-cost');
-    const clothInputText = clothInput.value;
-    const clothCost =parseFloat(clothInputText);
-    foodInput.value=clothCost;
-
-})
+    
+    
+     const totalCost = parseFloat(foodInput.value)+parseFloat(rentInput.value)+parseFloat(clothInput.value);
+     totalCostInput.innerText=totalCost;
+    
+    
+    
+    
+});
